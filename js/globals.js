6c6e13ce6e1cdd3a9fde2c4fa16563bc7287c0f2
ShaLi Shaltiel <shali.shaltiel@jivesoftware.com>
2014-05-13 11:45:19 +0300

    moved service to jewbox

--- js/globals.js
+++ js/globals.js
@@ -5,4 +5,4 @@ var stoped = false;
 var milliPerAnnouncement = 10000;
 var milliBetweenAnnouncementsFetch = 120000;
 var milliForFireapp = 10000;
-var server = "http://jewbox:1337";
+var server = "http://10.72.110.2:1337";
