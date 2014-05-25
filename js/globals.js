3225adf3294007058eb8f3bb8c5a50e633ab0665
ShaLi Shaltiel <shali.shaltiel@jivesoftware.com>
2014-05-25 15:54:29 +0300

    added debug mode

--- js/globals.js
+++ js/globals.js
@@ -1,6 +1,7 @@
 /**
  * Created by hadargeva on 5/12/14.
  */
+var debug = window.location.href.indexOf("#debug") != -1;
 var showingJive = true;
 var milliPerAnnouncement = 10000;
 var milliPerInstagram = 5000;
@@ -8,6 +9,14 @@ var milliBetweenAnnouncementsFetch = 120000;
 var milliForFireapp = 20000;
 var milliForJiveIsrael = 60000;
 var server = "http://10.72.110.2:1337";
+
+if (debug) {
+    milliPerAnnouncement = milliPerAnnouncement / 10;
+    milliPerInstagram = milliPerInstagram / 10;
+    milliBetweenAnnouncementsFetch = milliBetweenAnnouncementsFetch / 10;
+    milliForFireapp = milliForFireapp / 10;
+    milliForJiveIsrael = milliForJiveIsrael / 10;
+}
 
 setTimeout(toggleViews, milliForJiveIsrael);
 
